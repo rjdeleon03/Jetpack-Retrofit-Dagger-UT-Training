c27ad3ea2adba1fca0a600e3dce63fb6e6fae497
rjdeleon03 <rjdeleon@up.edu.ph>
2020-01-31 10:44:47 +0800

    Enable back button

--- app/src/main/java/com/rjdeleon/animals/view/MainActivity.kt
+++ app/src/main/java/com/rjdeleon/animals/view/MainActivity.kt
@@ -2,12 +2,24 @@ package com.rjdeleon.animals.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.rjdeleon.animals.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mNavController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mNavController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, mNavController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(mNavController, null)
     }
 }
