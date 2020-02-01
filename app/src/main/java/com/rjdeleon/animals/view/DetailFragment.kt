d31b88b570e0ba98f7bd31fb345b454f85e2c52b
rjdeleon03 <rjdeleon@up.edu.ph>
2020-02-01 20:46:16 +0800

    Use palette to set details bg color

--- app/src/main/java/com/rjdeleon/animals/view/DetailFragment.kt
+++ app/src/main/java/com/rjdeleon/animals/view/DetailFragment.kt
@@ -1,12 +1,17 @@
 package com.rjdeleon.animals.view
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 import com.rjdeleon.animals.R
 import com.rjdeleon.animals.model.Animal
@@ -45,6 +50,28 @@ class DetailFragment : Fragment() {
         animalLifespan.text = mAnimal?.lifeSpan
         animalDiet.text = mAnimal?.diet
 
+        mAnimal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }
+
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val intColor = palette?.lightVibrantSwatch?.rgb ?: 0
+                            animalLayout.setBackgroundColor(intColor)
+                        }
+                }
+            })
     }
 
 }
